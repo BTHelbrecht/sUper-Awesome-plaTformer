65fb3b4e01236bec7b76d4bb8caf58e2e94aa1fa
Jerard Carney <jercarne@uat.edu>
2018-12-06 02:43:04 -0700

    1.4
    Final revise

---
+++ sUper Awesome plaTformer/Assets/Scripts/UI/nextLevel3.cs
@@ -0,0 +1,22 @@
+﻿
+// JERARD CARNEY
+// 12.2.18
+
+// Libraries
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Publlic Class playGame MonoBehaviour
+public class nextLevel3 : MonoBehaviour
+{
+
+    // Triggers collider of computer sprite at the ent to go to next Scene
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("Level_003");
+    }
+
+}
+
